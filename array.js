59a1cb9a4afabc3b322315078a5a42f9ad7e80b9
Samiksha <samikshyabhandari592@gmail.com>
2025-09-05 07:39:27 +0545

    array practice

---
+++ array.js
@@ -0,0 +1,18 @@
+// let arr=["laptop","Phone","TV"];
+// console.log(typeof(arr));
+
+// const student=[];
+// student[0]="Samiksha";
+// student[1]="Nirjala";
+// student[2]="Samina";
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr.length-1);
+// console.log(arr.toString());
+
+let Arr=[1,2,3,4,5];
+ console.log(Arr.toString());
+
+for(let i=0;i<Arr.length;i++){
+    console.log(Arr[i]);
+}
