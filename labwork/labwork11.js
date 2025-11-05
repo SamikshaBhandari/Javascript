563aafb2814daf0756df31a1738bde66cb627f68
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2025-11-05 12:13:13 +0545

    labwork11 update

---
+++ labwork/labwork11.js
@@ -0,0 +1,6 @@
+// Write a javascript program to create an array of strings and use numeric sorting to 
+// sort the strings in alphabetical order.print the sorted array.
+
+let strArray=["Orange","Apple","Banana","Avogardo","Kiwi"];
+strArray.sort();
+console.log(strArray);
