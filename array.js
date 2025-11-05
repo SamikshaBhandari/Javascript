ade54a94a3a1ffc2c39df49fd33e513130ed88d1
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2025-11-05 19:32:13 +0545

    Refactor array operations and enhance animal array logging

--- array.js
+++ array.js
@@ -16,3 +16,17 @@ let Arr=[1,2,3,4,5];
 for(let i=0;i<Arr.length;i++){
     console.log(Arr[i]);
 }
+// let poped=Arr.pop();
+// console.log(Arr);
+// let push=Arr.push(6);
+// console.log(Arr);
+// let shiftelement=Arr.shift();
+// console.log(Arr);
+// let unshiftelement=Arr.unshift(7);
+// console.log(Arr);
+
+let animals=["dog","cat","cow","lion"];
+// let spliced=animals.splice(2,1,"Donkey");
+ console.log(animals.toString());
+let spliced=animals.splice(1);
+console.log(spliced);
