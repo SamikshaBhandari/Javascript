f72a1fb17b825ac6e736218e1e5cc61d40e9c115
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2025-11-05 07:06:54 +0545

    map method

--- arrayforeach.js
+++ arrayforeach.js
@@ -5,3 +5,11 @@ array.forEach(num=>{
     console.log(num);
 })
 
+//map reduce and filter method in array 
+
+let arr=[3,4,5,6,7,8];
+let a=arr.map((value,index,a)=>{
+    console.log(value,index,a);
+    return value + index;
+})
+console.log(a);
