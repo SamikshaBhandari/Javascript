0e2c3479b4234544182b2e1c5d72c774e47d88c0
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2025-11-12 20:01:55 +0545

    Labwork

--- labwork/even.js
+++
@@ -1,3 +0,0 @@
-//Write a javascript program to create an array of
-//  numbers and use forEach()to print all the evennumbers in the array.
-
--- labwork/hello.js
+++
@@ -1,5 +0,0 @@
-//1.WAp a basic javascript 'Hello World'With berif introduction of 
-// javascript and javascript coding conventions.
-
-let Name="Hello World";
-console.log(Name);
